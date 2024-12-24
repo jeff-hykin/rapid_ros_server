4f40996b25b6afa70b52b9702b4fc9ed280c8193
Jeff Hykin <jeff.hykin@gmail.com>
2024-12-24 17:18:54 -0500

    use esm imports

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-import { Elemental, passAlongProps } from "https://deno.land/x/elementalist@0.6.3/main/deno.js?code"
-import { css, components, Column, Row, askForFiles, Code, Input, Button, Checkbox, Dropdown, popUp, cx, } from "https://deno.land/x/good_component@0.2.12/elements.js"
-import { fadeIn, fadeOut } from "https://deno.land/x/good_component@0.2.12/main/animations.js"
-import { showToast } from "https://deno.land/x/good_component@0.2.12/main/actions.js"
-import { addDynamicStyleFlags, setupStyles, createCssClass, setupClassStyles, hoverStyleHelper, combineClasses, mergeStyles, AfterSilent, removeAllChildElements } from "https://deno.land/x/good_component@0.2.12/main/helpers.js"
-import { zip, enumerate, count, permute, combinations, wrapAroundGet } from "https://deno.land/x/good@1.5.1.0/array.js"
+import { Elemental, passAlongProps } from "https://esm.sh/gh/jeff-hykin/elemental@0.6.4/main/deno.js"
+import { css, components, Column, Row, askForFiles, Code, Input, Button, Checkbox, Dropdown, popUp, cx, } from "https://esm.sh/gh/jeff-hykin/good-component@0.3.0/elements.js"
+import { fadeIn, fadeOut } from "https://esm.sh/gh/jeff-hykin/good-component@0.3.0/main/animations.js"
+import { showToast } from "https://esm.sh/gh/jeff-hykin/good-component@0.3.0/main/actions.js"
+import { addDynamicStyleFlags, setupStyles, createCssClass, setupClassStyles, hoverStyleHelper, combineClasses, mergeStyles, AfterSilent, removeAllChildElements } from "https://esm.sh/gh/jeff-hykin/good-component@0.3.0/main/helpers.js"
+import { zip, enumerate, count, permute, combinations, wrapAroundGet } from "https://esm.sh/gh/jeff-hykin/good-js@1.13.5.1/source/array.js"
 
-import storageObject from "https://deno.land/x/storage_object@0.0.3.5/main.js"
+import storageObject from "https://esm.sh/gh/jeff-hykin/storage-object@0.0.3.5/main.js"
 
 const { html } = Elemental({
     ...components,
