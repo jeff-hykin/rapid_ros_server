4152374609e85f9d42c4a04a3706654efdd6e248
Jeff Hykin <jeff.hykin@gmail.com>
2025-01-01 11:43:02 -0500

    improve template

--- run/build.ps1
+++ run/build.ps1
@@ -6,6 +6,7 @@ echo --% >/dev/null;: ' | out-null
 # for not-Windows operating systems
 #
 deno run -A https://esm.sh/gh/jeff-hykin/bite@e070c4af1e/vite/bin/vite.js build
+# deno run -A https://deno.land/x/html_bundle/main/html-bundle.js --inplace ./docs/index.html 
 
 exit #>
 
@@ -13,3 +14,4 @@ exit #>
 # for windows (powershell)
 #
 deno run -A https://esm.sh/gh/jeff-hykin/bite@e070c4af1e/vite/bin/vite.js build
+# deno run -A https://deno.land/x/html_bundle/main/html-bundle.js --inplace ./docs/index.html 
