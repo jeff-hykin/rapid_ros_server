d73ffe8089ddcb9531b7571f90bd31951e969484
Jeff Hykin <jeff.hykin@gmail.com>
2025-03-04 16:12:59 -0600

    update archy

--- run/serve.ps1
+++ run/serve.ps1
@@ -5,11 +5,11 @@ echo --% >/dev/null;: ' | out-null
 #
 # for not-Windows operating systems
 #
-deno run --allow-net --allow-read --allow-write --allow-run --allow-sys https://deno.land/x/archaeopteryx/mod.ts dist/
+deno run --allow-net --allow-read --allow-write --allow-run --allow-sys https://raw.githubusercontent.com/jeff-hykin/archaeopteryx/80316971344a78b32f60708134ecc850b8083102/mod.ts dist/
 
 exit #>
 
 #
 # for windows (powershell)
 #
-deno run --allow-net --allow-read --allow-write --allow-run --allow-sys https://deno.land/x/archaeopteryx/mod.ts dist/
+deno run --allow-net --allow-read --allow-write --allow-run --allow-sys https://raw.githubusercontent.com/jeff-hykin/archaeopteryx/80316971344a78b32f60708134ecc850b8083102/mod.ts dist/
