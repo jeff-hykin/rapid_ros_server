ddd92d5fb96359d6842d4a2cf2df21dd40e8e34a
Jeff Hykin <jeff.hykin@gmail.com>
2025-03-13 12:27:17 -0500

    update bite version

--- run/build.ps1
+++ run/build.ps1
@@ -5,13 +5,13 @@ echo --% >/dev/null;: ' | out-null
 #
 # for not-Windows operating systems
 #
-deno run -A 'https://esm.sh/gh/jeff-hykin/bite@e070c4af1e/vite/bin/vite.js' build ./main --outDir ../docs
-# deno run -A https://esm.sh/gh/jeff-hykin/html-bundle@0.0.3.0/main/html-bundle.js --inplace ./docs/index.html
+deno run -A 'https://esm.sh/gh/jeff-hykin/bite@5.4.10.2/vite/bin/vite.js' build ./main --outDir ../docs --emptyOutDir
+deno run -A https://esm.sh/gh/jeff-hykin/html-bundle@0.0.3.0/main/html-bundle.js --inplace ./docs/index.html
 
 exit #>
 
 #
 # for windows (powershell)
 #
-deno run -A 'https://esm.sh/gh/jeff-hykin/bite@e070c4af1e/vite/bin/vite.js' build ./main --outDir ../docs
-# deno run -A https://esm.sh/gh/jeff-hykin/html-bundle@0.0.3.0/main/html-bundle.js --inplace ./docs/index.html
+deno run -A 'https://esm.sh/gh/jeff-hykin/bite@5.4.10.2/vite/bin/vite.js' build ./main --outDir ../docs --emptyOutDir
+deno run -A https://esm.sh/gh/jeff-hykin/html-bundle@0.0.3.0/main/html-bundle.js --inplace ./docs/index.html
