68e4666e2aaeccf8105a3a44d6181ad5e3e1bc92
Jeff Hykin <jeff.hykin@gmail.com>
2025-04-20 12:16:19 -0500

    add optimize arguments

--- run/build.ps1
+++ run/build.ps1
@@ -5,7 +5,7 @@ echo --% >/dev/null;: ' | out-null
 #
 # for not-Windows operating systems
 #
-deno run -A 'https://esm.sh/gh/jeff-hykin/bite@5.4.10.2/vite/bin/vite.js' build ./main --outDir ../docs --emptyOutDir
+deno run -A 'https://esm.sh/gh/jeff-hykin/bite@5.4.10.2/vite/bin/vite.js' build ./main --target esnext --mode production --minify --emptyOutDir --outDir ../docs
 deno run -A https://esm.sh/gh/jeff-hykin/html-bundle@0.0.3.0/main/html-bundle.js --inplace ./docs/index.html
 
 exit #>
@@ -13,5 +13,5 @@ exit #>
 #
 # for windows (powershell)
 #
-deno run -A 'https://esm.sh/gh/jeff-hykin/bite@5.4.10.2/vite/bin/vite.js' build ./main --outDir ../docs --emptyOutDir
+deno run -A 'https://esm.sh/gh/jeff-hykin/bite@5.4.10.2/vite/bin/vite.js' build ./main --target esnext --mode production --minify --emptyOutDir --outDir ../docs
 deno run -A https://esm.sh/gh/jeff-hykin/html-bundle@0.0.3.0/main/html-bundle.js --inplace ./docs/index.html
